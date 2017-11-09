fdcf4640ba2f4e50a9776637da5bb2b00cc524ad
William Brooks <brooks.william@gmail.com>
2017-11-08 16:02:42 -0800

    need to add input checking

--- letter.js
+++ letter.js
@@ -1,18 +1,16 @@
-
-function Letter(character) {
-	this.character = character;
-	this.guessed = false;
-	this.isSpace = false;
-	this.getPlaceholder = function () {
-		return "-";
-	}
-	this.getCharacter = function() {
-		return this.character;
-	}
-	this.spaceCharacter = function () {
-		return " ";
-	}
+module.exports = {
+    Letter: function Letter(character) {
+        this.character = character;
+        this.guessed = false;
+        this.isSpace = false;
+        this.getPlaceholder = function() {
+            return "-";
+        }
+        this.getCharacter = function() {
+            return this.character;
+        }
+        this.spaceCharacter = function() {
+            return " ";
+        }
+    }
 }
-
-
-module.exports = Letter;
